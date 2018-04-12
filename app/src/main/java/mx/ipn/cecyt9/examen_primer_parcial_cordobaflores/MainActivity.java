21df4d18a6bc534ffa033dc7aee56b39c973fea0
Córdoba Flores Claudia <claudiscordoba92@gmail.com>
2018-04-12 09:03:09 -0500

    Commit Inicial

--- app/src/main/java/mx/ipn/cecyt9/examen_primer_parcial_cordobaflores/MainActivity.java
+++ app/src/main/java/mx/ipn/cecyt9/examen_primer_parcial_cordobaflores/MainActivity.java
@@ -2,12 +2,49 @@ package mx.ipn.cecyt9.examen_primer_parcial_cordobaflores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String Usuario = "Claudia";
+    String Password = " 123";
+    String UsuarioObtenido = "";
+    String PasswordObtenido = "";
+    EditText tv, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (EditText) findViewById(R.id.editText);
+        tv2 = (EditText) findViewById(R.id.editText2);
+
+
+
+    }
+    public void validación(View v){
+        UsuarioObtenido = tv.getText().toString();
+        PasswordObtenido = tv2.getText().toString();
+        if(Usuario.equals(UsuarioObtenido) && Password.equals(PasswordObtenido)){
+            //mandar a llamar la actividad correcta
+            Toast toast1=
+                    Toast.makeText(getApplicationContext(),
+
+                             "Usuario Correcto",Toast.LENGTH_SHORT);
+            toast1.show();
+
+        }
+        else{
+            //si los valores no coinciden mandar a llamar la actividad de error
+            Toast toast2=
+                    Toast.makeText(getApplicationContext(),
+                            "Usuario Correcto",Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+
     }
 }
+
